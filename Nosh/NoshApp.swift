e08ef09dbe0f37e2dabab2e81771ffa344718d00
ashmit <ashmit27j@gmail.com>
2025-07-10 16:18:28 +0530

    added firebase dependencies, havent added uikit implementation yet

--- Nosh/NoshApp.swift
+++ Nosh/NoshApp.swift
@@ -1,7 +1,13 @@
 import SwiftUI
+import FirebaseCore
+import FirebaseFirestore
+import FirebaseAuth
 
 @main
 struct NoshApp: App {
+    // Register the custom AppDelegate
+    @UIApplicationDelegateAdaptor(AppDelegate.self) var delegate
+
     var body: some Scene {
         WindowGroup {
             SplashScreen()
@@ -9,3 +15,11 @@ struct NoshApp: App {
     }
 }
 
+// AppDelegate to configure Firebase
+class AppDelegate: NSObject, UIApplicationDelegate {
+    func application(_ application: UIApplication,
+                     didFinishLaunchingWithOptions launchOptions: [UIApplication.LaunchOptionsKey : Any]? = nil) -> Bool {
+        FirebaseApp.configure()
+        return true
+    }
+}
