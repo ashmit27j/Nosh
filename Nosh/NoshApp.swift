80da34b75eea7e5895c02965073b8968f025d280
ashmit <ashmit27j@gmail.com>
2025-07-20 01:51:38 +0530

    latest safe commit - 20/7/2025 - Release alpha 0.1

--- Nosh/NoshApp.swift
+++ Nosh/NoshApp.swift
@@ -1,13 +1,8 @@
 import SwiftUI
-import FirebaseCore
-import FirebaseFirestore
-import FirebaseAuth
+
 
 @main
 struct NoshApp: App {
-    // Register the custom AppDelegate
-    @UIApplicationDelegateAdaptor(AppDelegate.self) var delegate
-
     var body: some Scene {
         WindowGroup {
             SplashScreen()
@@ -15,11 +10,4 @@ struct NoshApp: App {
     }
 }
 
-// AppDelegate to configure Firebase
-class AppDelegate: NSObject, UIApplicationDelegate {
-    func application(_ application: UIApplication,
-                     didFinishLaunchingWithOptions launchOptions: [UIApplication.LaunchOptionsKey : Any]? = nil) -> Bool {
-        FirebaseApp.configure()
-        return true
-    }
-}
+
